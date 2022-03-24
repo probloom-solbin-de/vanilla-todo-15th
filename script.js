2784245f178ab9dcba0c22f38c6b0abc752b8ae4
probloom-solbin-de <101960628+probloom-solbin-de@users.noreply.github.com>
2022-03-24 15:53:17 +0900

    Test 1
    기능이 제대로 작동하는지 확인하기 위함.

--- script.js
+++ script.js
@@ -19,29 +19,33 @@ let todo = [];
 
 /* click 이벤트 발생 시에 input을 todo-list에 저장 */
 
-const function MakeTodo(todoText,isItDone){
+const function makeTodo(todoText,isItDone){
+    console.log("makeTodo is running.");
     return {
         todoText,
         isItDone
     }
 }
 
-const function addNewTodo(){
-    if (!todoInput.value) {
-        alert('입력한 내용이 없어요!');
-    } else {
-            todo.push(MakeTodo(todoInput.value, false));
-        }
-    }
-    showTask();
-};
-
 const function showTask(){
+    console.log("showTask is running.");
     todo.forEach(todoText){
         newTodo = <li>${todoText}</li>
     }
     todoList.innerHTML = newTodo;
 }
+
+const function addNewTodo(){
+    console.log("addNewTodo is running.");
+    if (!todoInput.value) {
+        alert('입력한 내용이 없어요!');
+    } else {
+            console.log("todo is pushed.");
+            todo.push(makeTodo(todoInput.value, false));
+        }
+    }
+    showTask();
+};
 
 /* click 이벤트 발생 시에 input을 todo-list에 저장 */
 
